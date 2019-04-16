60adeb07ecd6bea1f385bcb53983769879a01dca
doudoukeke <729181989@qq.com>
2019-04-16 22:36:23 +0800

    tijiao

---
+++ login/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from login import models
+# from login.models import UserInfo
+# Create your views here.
+def index(request):
+
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get("password")
+        #将数据保存到数据库
+        models.UserInfo.objects.create(user=username,pwd=password)
+
+    #从数据库中读取所有数据，注意缩进
+    user_list=models.UserInfo.objects.all()
+    return render(request,'index.html',{'data':user_list})
